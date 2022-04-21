e161fa426e3655786926498aaff0748ea0286718
quocngo-git <ngoquoc.work@gmail.com>
2022-04-21 14:19:45 -0700

    add main

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
 import java.net.*;
 import java.io.*;
 public class Main {
-    EchoServer server = new EchoServer();
-    server.start(5000);
+    public static void main(String[] args){
+        EchoServer server = new EchoServer();
+        server.start(5000);
+    }
+    
 }
+
